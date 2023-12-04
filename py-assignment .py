aceef3a0acfb09475bfc9d7aa226b07ca2488ead
Sophie Zheng <139349595+921zheng@users.noreply.github.com>
2023-12-04 11:57:46 +0000

    Add files via upload

---
+++ py-assignment .py
@@ -0,0 +1,68 @@
+#1.Write a python code to Find whether the person is eligible to vote or not
+
+# age=23
+# if age>=18:
+#     print("you can vote")
+# else:
+#     print("go home")
+
+#2. Write a python code to Find whether a Character is vowel or consonant 
+
+# char=input("say alphebet")
+# list=["a","e","i","o","u"]
+# if char.lower() in list:
+#     print("vowel")
+# else:
+#     print("consonant ")
+
+# #3. Write a python code to Find the sum of first 5 integer numbers 
+# num=input("say a random number more than 5 digits")
+# sum=0
+
+# for i in range(5):
+#     sum+=int(num[i])
+# print(sum)
+
+
+# 4. Write a python code to Find the 10 multiple for 5 using while loop 
+#     Eg : 5 , 10 ,15, 20 ,25, 30 ,35, 40, 45, 50
+
+# print([num*5 for num in range(1,11)])
+    
+#5. Write a python code to find the 1st 10 odd numbers using range function 
+
+# print([num for num in range(1,21) if num%2!=0])
+
+# 6. Write a python code to check whether the grade of student 
+#      >90 ---A Grade
+#      >80----B Grade
+#      >70----C Grade
+#      >60----D Grade
+#      <50----fail
+
+# grade=88
+# if grade>90:
+#     print("A")
+# elif grade>80:
+#     print("B")
+# elif grade>70:
+#     print("C")
+# elif grade>60:
+#     print("D")
+# else:
+#     print("fail")
+
+#7. Write a python code to loop through string using for loop
+
+# str="fhslfjs"
+# for i in str:
+#     print(i)
+
+
+
+
+
+
+
+
+
